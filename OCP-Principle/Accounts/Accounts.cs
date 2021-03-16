39e025c782ff0ea3453512ae3710d9de7e98d29f
Paulo Serafim <paserafim@gmail.com>
2021-03-16 18:09:34 +0000

    Adding interfaces for refactoring code to respect Open-Closed principle.

--- OCP-Principle/Accounts/Accounts.cs
+++ OCP-Principle/Accounts/Accounts.cs
@@ -13,25 +13,6 @@ namespace OCP_Principle
             output.LastName = person.LastName;
             output.EmailAddress = $"{person.FirstName.Substring(0, 1)}{person.LastName}@mymail.com";
 
-            //if (person.TypeOfEmployee == EmployeeType.Manager)
-            //{
-            //    // violating OCP
-            //    output.IsManager = true;
-            //}
-
-            //switch (person.TypeOfEmployee)
-            //{
-            //    case EmployeeType.Staff:
-            //        break;
-            //    case EmployeeType.Manager:
-            //        output.IsManager = true;
-            //        break;
-            //    case EmployeeType.Executive:
-            //        output.IsManager = true;
-            //        output.IsExecutive = true;
-            //        break;
-            //}
-
             return output;
         }
     }
---
+++ OCP-Principle/Applicants/TechnicianModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCP_Principle
+{
+    public class TechnicianModel : IApplicantModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IAccounts AccountProcessor { get; set; } = new Accounts();
+    }
+}
