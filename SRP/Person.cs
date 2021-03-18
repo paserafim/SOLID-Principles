26d2a8b6ee3aa3c08bce7bdd4a7d476bf9c0e557
Paulo Serafim <paserafim@gmail.com>
2021-03-18 11:29:19 +0000

    Single Responsability Principle Initial Commit.

---
+++ SRP/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SRP
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
---
+++ SRP/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SRP
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my application");
+
+            // Ask for user information
+            Person user = new Person();
+
+            Console.WriteLine("What is your first name:");
+            user.FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name:");
+            user.LastName = Console.ReadLine();
+
+
+            // Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                Console.ReadLine();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(user.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                Console.ReadLine();
+                return;
+            }
+
+            // create a username for the person
+
+        }
+    }
+}
