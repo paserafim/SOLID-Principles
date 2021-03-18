464eaebc18a2130ac7d8945b79c175c81e0261d6
Paulo Serafim <paserafim@gmail.com>
2021-03-18 11:45:30 +0000

    Create PersonValidator class.

---
+++ SRP/PersonValidator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRP
+{
+    public static class PersonValidator
+    {
+        public static bool Validate(Person person)
+        {
+            // Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                Console.WriteLine("You did not give us a valid first name!");
+                StandardMessages.EndApplication();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(person.LastName))
+            {
+                Console.WriteLine("You did not give us a valid last name!");
+                StandardMessages.EndApplication();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- SRP/Program.cs
+++ SRP/Program.cs
@@ -11,22 +11,22 @@ namespace SRP
             Person user = PersonDataCapture.Capture();
 
 
-            // Checks to be sure the first and last names are valid
-            if (string.IsNullOrWhiteSpace(user.FirstName))
-            {
-                Console.WriteLine("You did not give us a valid first name!");
-                //Console.ReadLine();
-                StandardMessages.EndApplication();
-                return;
-            }
+            //// Checks to be sure the first and last names are valid
+            //if (string.IsNullOrWhiteSpace(user.FirstName))
+            //{
+            //    Console.WriteLine("You did not give us a valid first name!");
+            //    //Console.ReadLine();
+            //    StandardMessages.EndApplication();
+            //    return;
+            //}
 
-            if (string.IsNullOrWhiteSpace(user.LastName))
-            {
-                Console.WriteLine("You did not give us a valid last name!");
-                //Console.ReadLine();
-                StandardMessages.EndApplication();
-                return;
-            }
+            //if (string.IsNullOrWhiteSpace(user.LastName))
+            //{
+            //    Console.WriteLine("You did not give us a valid last name!");
+            //    //Console.ReadLine();
+            //    StandardMessages.EndApplication();
+            //    return;
+            //}
 
             // create a username for the person
             Console.WriteLine($"Your username is {user.FirstName.Substring(0,1)}{user.LastName}");
